51a27cf632817a9fa2624fcc94e69a53023ab66b
root <root@DESKTOP-UEO62DE.localdomain>
2024-01-14 14:01:02 +0300

    Validation ad constraints

--- server/models.py
+++ server/models.py
@@ -1,16 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
 
 class EmailAddress(db.Model):
     __tablename__ = 'emailaddress'
+    
+    id = Column(Integer, primary_key = True)
+    email = Column(String)
+    backup_email = Column(String)
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String)
-    backup_email = db.Column(db.String)
+    @validates('email', 'backuo_email')
+    def validate_email(self, key, adress):
+        if '@' not in adress:
+            raise ValueError ("failed simple email validation")
+        return adress
+email = EmailAddress(email = 'banana')
+Session = session.add(email)
+session = Session()
 
-    @validates('email', 'backup_email')
-    def validate_email(self, key, address):
-        if '@' not in address:
-            raise ValueError("Failed simple email validation")
-        return address
+base.metadata.create_all(db)
+
+email = EmailAddress(email='banana')
+session.add(email)
+
+try:
+    session.commit()
+except sqlalchemy.exc.IntegrityError as e:
+    print("Integrity violation blocked!")
+    session.rollback()
+    
+
+
